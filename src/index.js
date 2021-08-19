1c3dd1454f502879c4b6f0a2172563a2ed4c34ef
guillaume-debeire <guillaume@debeire.com>
2021-08-19 18:35:49 +0200

    init

--- src/index.js
+++ src/index.js
@@ -4,13 +4,35 @@ import { render } from 'react-dom';
 
 // == Import : local
 // Composants
-import Recipe from './components/Recipe';
+
+function Header() {
+  return (
+    <header>
+      <h1>Recette de crêpes</h1>
+    </header>
+  );
+}
 
 // == Render
-// 1. Élément React racine (celui qui contient l'ensemble de l'app)
-//    => crée une structure d'objets imbriqués (DOM virtuel)
-const rootReactElement = <Recipe />;
-// 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
+// 1. Elément React racine (celui qui contient l'ensemble de l'app)
+//    => crée une structure d'objets imbriqués (DOM Virtuel)
+const rootReactElement = (
+  <div>
+    <Header />
+    <ul>
+      <li>Ingrédient 1</li>
+      <li>Ingrédient 2</li>
+      <li>Ingrédient 3</li>
+    </ul>
+    <ul>
+      <li>Ingrédient 1</li>
+      <li>Ingrédient 2</li>
+      <li>Ingrédient 3</li>
+    </ul>
+  </div>
+);
+
+// 2 La cible du DOM (la où la structure doit prendrevie dans le DOM)
 const target = document.getElementById('root');
 // 3. Déclenchement du rendu de React (virtuel) => DOM (page web)
 render(rootReactElement, target);
